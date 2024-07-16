ae93b1d5e6902255995068fcfeae6d40b995ecad
Kevin <contact@kevinpaulidor.de>
2024-07-16 19:02:34 +0200

    chore: Update friends.js to send the friends object as response

--- router/friends.js
+++ router/friends.js
@@ -12,9 +12,8 @@ let friends = {
 // GET request: Retrieve all friends
 router.get("/",(req,res)=>{
 
-  // Update the code here
-
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  // send the friends object as response
+  res.send(friends)
 });
 
 // GET by specific ID request: Retrieve a single friend with email ID
