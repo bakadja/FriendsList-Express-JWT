fc7092cf298ba5266e7a3ad2d4be17919cd38561
Kevin <contact@kevinpaulidor.de>
2024-07-16 19:39:16 +0200

    chore: Update POST request in friends.js to add a new friend

--- router/friends.js
+++ router/friends.js
@@ -19,7 +19,7 @@ router.get("/",(req,res)=>{
 // GET by specific ID request: Retrieve a single friend with email ID
 router.get("/:email",(req,res)=>{
   const { email } = req.params;
-  
+
   console.log(email)
   const friend = friends[email];
   if(friend){
@@ -32,10 +32,21 @@ router.get("/:email",(req,res)=>{
 
 
 // POST request: Add a new friend
-router.post("/",(req,res)=>{
-  // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+ router.post("/", (req, res) => {
+
+    // Check if email is provided in the request body
+    if (req.body.email) {
+        // Create or update friend's details based on provided email
+        friends[req.body.email] = {
+            "firstName": req.body.firstName,
+            "lastName": req.body.lastName,
+            "DOB": req.body.DOB
+        };
+    }
+    // Send response indicating user addition
+    res.send("The user" + (' ') + (req.body.firstName) + " Has been added!");
 });
+
 
 
 // PUT request: Update the details of a friend with email id
